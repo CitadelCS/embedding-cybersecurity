9f0dfddb32abb451686c2034cce34d582e277ee4
Michael Verdicchio <verdicchio@gmail.com>
2016-03-31 15:43:13 -0400

    improved exceptions toy example

--- HandleExceptions.java
+++ HandleExceptions.java
@@ -1,18 +1,23 @@
 package edu.citadel.cs.ecc.cs2;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class HandleExceptions {
 
   public static void main(String[] args) {
+    // PART 1
     try {
       doSomethingDangerous();
     } catch (Exception e) {
-      System.out.println("Nothing to see here. Move along.");
+      System.out.println("PART 1: Nothing to see here. Move along.");
     }
-    doSomethingLessDangerous();
+    // PART 2
+    try {
+      doSomethingLessDangerous();
+    } catch (Exception e) {
+      System.out.println("PART 2: Nothing to see here. Move along.");
+    }
   }
 
   public static void doSomethingDangerous() throws IOException {
@@ -23,7 +28,7 @@ public class HandleExceptions {
   public static void doSomethingLessDangerous() {
     try {
       Scanner fileScan = new Scanner(new File("this doesn't exist.txt"));
-    } catch (IOException ioe) {
+    } catch (FileNotFoundException e) {
       System.out.println("Bad file name.");
     }
   }
